d4d2c74cb6b3766f5e11de8093ac9fc35b2d3143
chobkyu <98572357+chobkyu@users.noreply.github.com>
2024-01-05 16:49:10 +0900

    first commit

---
+++ src/main/kotlin/com/example/demo/blog/repository/repositories.kt
@@ -0,0 +1,8 @@
+package com.example.demo.blog.repository
+
+import com.example.demo.blog.entity.Wordcount
+import org.springframework.data.repository.CrudRepository
+
+interface WordRepository : CrudRepository<Wordcount,String> {
+    fun findTop10ByOrderByCntDesc() : List<Wordcount>
+}
---
+++ src/main/kotlin/com/example/demo/core/exception/InvalidInputException.kt
@@ -0,0 +1,7 @@
+package com.example.demo.core.exception
+
+class InvalidInputException(
+    message: String = "Invalid Input"
+) : RuntimeException(message
+) {
+}
